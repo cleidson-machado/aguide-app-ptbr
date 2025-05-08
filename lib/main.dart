a637b5dfa78b85a6c5d3b39ec9146a4c4a755055
Cleidson <cleidson.mac@outlook.com>
2025-05-08 19:21:32 +0100

    Just removing the .env depency to the main app code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+//import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:portugal_guide/app/app_custom_main_widget.dart';
 import 'package:portugal_guide/app/theme/app_theme_provider_full.dart';
 import 'package:provider/provider.dart';
